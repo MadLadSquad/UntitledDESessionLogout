0a27212d42fafbb0586dec31e8b3d6b6b3ecb546
Madman10K <stanv.contact176@gmail.com>
2024-06-26 23:34:03 +0300

    New CMakeLists

---
+++ Config/cmake/ude-session-logout.cmake
@@ -0,0 +1,11 @@
+function(custom_setup_step)
+endfunction()
+
+function(custom_compile_step)
+endfunction()
+
+set(USE_OS_MODULE ON)
+set(ENABLE_DBUS_UTILS ON)
+
+set(USE_THEME_MODULE ON)
+set(USE_I18N_MODULE ON)
